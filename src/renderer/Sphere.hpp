4186a4022d4524aca03445392be1e83b7dca1aed
Reece Holmdahl <holmd353@umn.edu>
2022-02-01 11:04:19 -0600

    Added Sphere dummy class derived from Shape, will implement later Working on reorganizing renderer library structure to have subdirectories

---
+++ src/renderer/Sphere.hpp
@@ -0,0 +1,20 @@
+#ifndef _SPHERE_REECE_
+#define _SPHERE_REECE_
+
+#include "Shape.hpp"
+#include "Vector3.hpp"
+
+class Sphere : public Shape
+{
+public:
+  Sphere() = default;
+  Sphere(const Vec3d &position, const double radius);
+
+  virtual bool closestHit(const Ray &r) const;
+
+private:
+  Vec3d m_position;
+  double m_radius;
+};
+
+#endif
