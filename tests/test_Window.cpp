a8f413e6a604ab1927e815d7460ddd0ecd884843
Reece Holmdahl <holmd353@umn.edu>
2022-02-03 21:01:27 -0600

    Made a misc test which just generates the sphere normal Finished test_Window, launches a window with -wh size args and generates a sphere normal and displays it

--- tests/test_Window.cpp
+++ tests/test_Window.cpp
@@ -3,115 +3,56 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
-
-#include "handleGraphicsArgs.h"
-
 #include "renderer.hpp"
+#include "Camera.hpp"
+#include "PerspectiveCamera.hpp"
+#include "CoordinateSys.hpp"
 #include "Framebuffer.hpp"
 #include "Vector3.hpp"
 #include "Sphere.hpp"
 #include "Ray.hpp"
-#include "Camera.hpp"
-#include "PerspectiveCamera.hpp"
-#include "CoordinateSys.hpp"
+
+#include "handleGraphicsArgs.h"
 
 using namespace renderer;
 
-GLFWwindow *window;
-GLuint texture;
-Camera *camera;
-size_t nx, ny;
-float *pixels;
+void createImage(GLuint &textureId, float *pixels, const size_t nx, const size_t ny, Camera *cam, Framebuffer (*gen_fb)(size_t, size_t, Camera *));
 
 Framebuffer sphereNormal(const size_t nx, const size_t ny, Camera *cam);
 
-void draw()
-{
-    // Render code goes here
-    std::cout << "before render" << std::endl;
-    // Framebuffer fb(sphereNormal(nx, ny, camera));
-    Framebuffer fb(nx, ny);
-    fb.clearColor(Vec3f(0.5, 0.5, 0));
-    std::cout << "after render" << std::endl;
-
-    framebufferToGLPixelArray(fb, pixels);
-    std::cout << "after pixel conversion" << std::endl;
-
-    // Debug print all pixels
-    // for (size_t i(0); i < 3 * nx * ny; ++i)
-    // {
-    //     std::cout << pixels[i] << " ";
-    //     if (i % 3 == 2)
-    //         std::cout << std::endl;
-    // }
-
-    glLoadIdentity(); // Load identity matrix? Does this need to be here?
-
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, nx, ny, 0, GL_RGB, GL_FLOAT, pixels);
-
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLE_STRIP);
-    glTexCoord2d(0.0, 0.0);
-    glVertex2d(-1.0, -1.0);
-    glTexCoord2d(1.0, 0.0);
-    glVertex2d(1.0, -1.0);
-    glTexCoord2d(1.0, 1.0);
-    glVertex2d(1.0, 1.0);
-    glTexCoord2d(0.0, 1.0);
-    glVertex2d(-1.0, 1.0);
-    glEnd();
-
-    glfwSwapBuffers(window);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
 int main(int argc, char *argv[])
 {
+    // Process cmdline args
+    sivelab::GraphicsArgs args;
+    args.process(argc, argv);
+
+    size_t nx(args.width), ny(args.height);
+
+    // Initialize the library
     if (!glfwInit())
     {
         return -1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window;
+    Camera *camera;
+    Framebuffer fb(nx, ny);
+    float *pixels = new float[3 * nx * ny];
+    float *pixelsOut = new float[3 * nx * ny];
+    GLuint textureId;
 
-    sivelab::GraphicsArgs args;
-    args.process(argc, argv);
-
-    // Used cmdline arguments
-    nx = args.width;
-    ny = args.height;
-
-    pixels = new float[3 * nx * ny];
-
-    int windowWidth(nx);
-    int windowHeight(ny);
-    int aspectRatio = nx / ny;
-
-    window = glfwCreateWindow(windowWidth, windowHeight, "test_Window", NULL, NULL);
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(nx, ny, "Hello World", NULL, NULL);
 
     if (!window)
     {
-        std::cerr << "GLFW did not create the window!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    glewExperimental = GL_TRUE;
     GLenum err(glewInit());
     if (err != GLEW_OK)
     {
@@ -119,103 +60,135 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
+    float vertices[] =
+        {
+            1.0, 1.0, // top right corner
+            0.0, 1.0, // top left corner
+            0.0, 0.0, // bottom left corner
+            1.0, 0.0  // bottom right corner
+        };
 
-    int fb_w, fb_h;
-    glfwGetFramebufferSize(window, &fb_w, &fb_h);
-    glViewport(0, 0, fb_w, fb_h);
+    float texcoords[] =
+        {
+            1.0, 1.0, // top right corner
+            0.0, 1.0, // top left corner
+            0.0, 0.0, // bottom left corner
+            1.0, 0.0  // bottom right corner
+        };
 
-    //Create orthographic view
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(0, 0, -1.f, 1.f, 1.f, -1.f);
-    glMatrixMode(GL_MODELVIEW);
+    camera = new PerspectiveCamera("persp", CoordinateSys::GLOBAL, 1.0, nx, ny, 1.0, 1.0 * ny / nx);
+
+    glViewport(0, 0, nx, ny);        // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glMatrixMode(GL_PROJECTION);     // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+    glLoadIdentity();                // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    glOrtho(0., 1., 0., 1., 0., 1.); // essentially set coordinate system
+    glMatrixMode(GL_MODELVIEW);      // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity();                // same as above comment
+
+    glGenTextures(1, &textureId);
+
+    // Render code goes here
+    // std::cout << "before render" << std::endl;
+    // fb = sphereNormal(nx, ny, camera);
+    // // fb.clearColor(Vec3f(0.5, 0.5, 0));
+    // std::cout << "after render" << std::endl;
+
+    // framebufferToGLPixelArray(fb, pixels);
+    // std::cout << "after pixel conversion" << std::endl;
+
     glEnable(GL_TEXTURE_2D);
-    glLoadIdentity();
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    Vec3d u, v, w;
+    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, nx, ny, 0, GL_RGB, GL_FLOAT, pixels);
 
-    u.set(1, 0, 0);
-    w.set(0, 0, 1);
-    v = u.cross(w);
+    // glGetTexImage(GL_TEXTURE_2D, 0, GL_RGB, GL_FLOAT, pixelsOut);
+    // Debug print all pixels
+    // for (size_t i(0); i < 3 * nx * ny; ++i)
+    // {
+    //     std::cout << pixelsOut[i] << " ";
+    //     if (i % 3 == 2)
+    //         std::cout << std::endl;
+    // }
 
-    CoordinateSys basis(Vec3d(0, 0, 0), u, v, w);
-
-    camera = new PerspectiveCamera("persp", basis, 1.0, fb_w, fb_h, 1.0, 1.0 / aspectRatio);
-
-    // Generate texture object
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glPixelStorei(GL_PACK_ALIGNMENT, 1);
-    glGenTextures(1, &texture);
-
-    // Move to draw() ?
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    // float borderColor[] = {1.0f, 0.0f, 0.0f, 1.0f};
-    // glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // // Generate framebuffer object
-    // GLuint fboId;
-    // glGenFramebuffers(1, &fboId);
-    // glBindFramebuffer(GL_FRAMEBUFFER, fboId);
-
-    // // Attach the texture to FBO color attachment point
-    // glFramebufferTexture2D(GL_FRAMEBUFFER,       // 1. fbo target: GL_FRAMEBUFFER
-    //                        GL_COLOR_ATTACHMENT0, // 2. attachment point
-    //                        GL_TEXTURE_2D,        // 3. tex target: GL_TEXTURE_2D
-    //                        texture,              // 4. tex ID
-    //                        0);                   // 5. mipmap level: 0(base)
-
-    // // Switch back to GLFW framebuffer
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // // set rendering destination to FBO
-    // glBindFramebuffer(GL_FRAMEBUFFER, fboId);
-
-    // // clear buffers
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // // draw a scene to a texture directly
-
-    // // unbind FBO
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    //
-    // glBindTexture(GL_TEXTURE_2D, texture);
-
-    // Unbind texture
     glBindTexture(GL_TEXTURE_2D, 0);
 
-    draw();
+    createImage(textureId, pixels, nx, ny, camera, sphereNormal);
 
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        // Event polling
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render OpenGL here
+        glBindTexture(GL_TEXTURE_2D, textureId);
+        glEnableClientState(GL_VERTEX_ARRAY_EXT);    // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY); // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glVertexPointer(2, GL_FLOAT, 0, vertices);   // point to the vertices to be used
+        glTexCoordPointer(2, GL_FLOAT, 0, texcoords);
+        glDrawArrays(GL_QUADS, 0, 4);          // draw the vertixes
+        glDisableClientState(GL_VERTEX_ARRAY); // tell OpenGL that you're finished using the vertex arrayattribute
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY_EXT);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glTranslatef(0, 0, -15);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
         glfwWaitEvents();
 
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         {
-            glfwSetWindowShouldClose(window, GL_TRUE);
+            glfwTerminate();
         }
 
         if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
         {
-            std::cout << "Reload Render" << std::endl;
+            // Reload/regenerate image
+            std::cout << "Reload" << std::endl;
+
+            createImage(textureId, pixels, nx, ny, camera, sphereNormal);
         }
     }
 
     glfwTerminate();
+
     return 0;
+}
+
+void createImage(GLuint &textureId, float *pixels, const size_t nx, const size_t ny, Camera *cam, Framebuffer (*gen_fb)(size_t, size_t, Camera *))
+{
+    glBindTexture(GL_TEXTURE_2D, textureId);
+
+    // std::cout << "before render" << std::endl;
+    Framebuffer fb(gen_fb(nx, ny, cam));
+    // std::cout << "after render" << std::endl;
+
+    framebufferToGLPixelArray(fb, pixels);
+    // std::cout << "after pixel conversion" << std::endl;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, nx, ny, 0, GL_RGB, GL_FLOAT, pixels);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 Framebuffer sphereNormal(const size_t nx, const size_t ny, Camera *cam)
 {
     Framebuffer fb(nx, ny);
 
-    Sphere sph(Vec3d(0, 0, 2), 0.5);
+    Sphere sph(Vec3d(0, 0, -2), 0.5);
 
     for (size_t i(0); i < nx; ++i)
     {
