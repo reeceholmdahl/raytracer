66ceaac6b082489350491c45f3ef02296f1e187f
Reece Holmdahl <holmd353@umn.edu>
2022-02-28 16:15:04 -0600

    Changed LambertShader light energy summation to match textbook

--- renderer/shader/LambertShader.cpp
+++ renderer/shader/LambertShader.cpp
@@ -7,16 +7,16 @@ LambertShader::LambertShader(const Vec3f &ambient, const Vec3f &color)
 
 Vec3f LambertShader::apply(const HitStruct &hit) const
 {
-    Vec3f intensity;
+    Vec3f color;
     for (Light *light : *(hit.lights))
     {
         auto hitToLight((light->position() - hit.hitPoint()).unitize());
         auto normal(hit.shape->normal(hit.hitPoint()));
 
-        intensity += light->intensity() * std::max(0.0, normal.dot(hitToLight));
+        color += m_diffuse * light->intensity() * std::max(0.0, normal.dot(hitToLight));
     }
 
-    auto color = m_diffuse * intensity + m_diffuse * m_ambient;
+    color += m_diffuse * m_ambient;
     color.set(
         std::min(1.0f, color[0]),
         std::min(1.0f, color[1]),
